73e76cf19b708e0a4140ce786e3b8c93568c318b
thombanks <thomas.banks@wearecube3.com>
2017-05-03 17:05:02 +0100

    styling

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const TaskLister = new Vue({
 		addTask: function() {
 			this.hasTask++
 			//trim() is used to remove whitespace from both ends of a string
-			var task = this.newTask.trim();
+			var task = this.newTask.trim()
 			//if task is not an empty string
 			if (task) {
 				//Push an object containing the task to the taskList array
@@ -40,7 +40,7 @@ const TaskLister = new Vue({
 				})
 
 				//Reset newTask to an empty string so the input field is cleared
-				this.newTask = "";
+				this.newTask = ""
 			}
 		},
 		removeTask: function(task) {
@@ -65,20 +65,18 @@ const TaskLister = new Vue({
 		},
 		selectAll: function(task) {
 			//targetValue is set to the opposite of areAllSelected
-			var targetValue = this.areAllSelected ? false : true;
+			var targetValue = this.areAllSelected ? false : true
 			//we use a for loop to set the checked state of all items to the target value
 			for (var i = 0; i < this.taskList.length; i++) {
 				this.taskList[i].checked = targetValue
 			}
 		},
 		reverseOrder: function() {
-			console.log(this.taskList)
 			this.taskList.reverse()
 		},
 		sortByDate: function() {
-			console.log(this.taskList)
 			this.taskList.sort(function(a, b) {
-				return (a.added > b.added) ? 1 : ((b.added > a.added) ? -1 : 0);
+				return (a.added > b.added) ? 1 : ((b.added > a.added) ? -1 : 0)
 			})
 			this.taskList.reverse()
 		}
@@ -87,8 +85,8 @@ const TaskLister = new Vue({
 		areAllSelected: function() {
 			//Check if every checked property returns true and if there is at least one to-do item
 			return this.taskList.every(function(task) {
-				return task.checked;
-			}) && this.taskList.length > 0;
+				return task.checked
+			}) && this.taskList.length > 0
 		},
 	}
 })
